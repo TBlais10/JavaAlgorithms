c3ca7e8ae316e516bbc0ac5e4eb14ce506d4a802
TBlais10 <blaislighting@gmail.com>
2021-09-21 22:29:02 -0400

    Began w/ importing the day 10 starting code for the challenge. Will be examining the challenge as time permits.

---
+++ src/com/company/Day10.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Day10 {
+
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            int n = Integer.parseInt(scanner.nextLine().trim());
+
+            scanner.close();
+        }
+    }
+
+}
