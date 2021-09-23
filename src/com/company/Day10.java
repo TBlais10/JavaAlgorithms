3b03465113b6bdb59d4034fffb34a5a14741360a
TBlais10 <blaislighting@gmail.com>
2021-09-22 22:26:44 -0400

    Began making progress on the day 10 challenge of Hacker Rank. Stuck in how to break away when the loops finds a 0 in the binary and needs to continue looping thru to find the consecutive numbers.

--- src/com/company/Day10.java
+++ src/com/company/Day10.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day10 {
@@ -9,9 +10,36 @@ public class Day10 {
             Scanner scanner = new Scanner(System.in);
 
             int n = Integer.parseInt(scanner.nextLine().trim());
+//            String binaryNum = "";
+
+//            while(n > 0) {
+//                int remainder = n % 2;
+//                n = n/2;
+//                binaryNum += remainder;
+//            }
+
+            String binaryNum = Integer.toBinaryString(n);
+
+            System.out.println(binaryNum);
+
+            int constCount = 0;
+
+            for (int i = 0; i <binaryNum.length(); i++) {
+                if (i != 0){
+                    constCount++;
+                }
+                else{
+                    constCount = 0;
+                }
+            }
 
             scanner.close();
         }
     }
 
 }
+
+/*
+
+
+Many languages have built-in functions for converting numbers from decimal to binary. To convert an integer, , from decimal to a String of binary numbers in Java, you can use the Integer.toBinaryString(n) function.*/
