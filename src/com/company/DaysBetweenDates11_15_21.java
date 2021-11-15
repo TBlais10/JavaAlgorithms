83b8f6cfbb878c27870b956984881dcee26c7a2e
TBlais10 <blaislighting@gmail.com>
2021-11-15 18:56:36 -0500

    Completed vowels challenge

---
+++ src/com/company/DaysBetweenDates11_15_21.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Date;
+
+//https://edabit.com/challenge/3hdXjfJozQySRC3gE
+public class DaysBetweenDates11_15_21 {
+
+    public Date getDays (Date date1, Date date2){
+
+    }
+
+}
--- src/com/company/HowManyVowels11_15_21.java
+++ src/com/company/HowManyVowels11_15_21.java
@@ -1,10 +1,22 @@
 package com.company;
+
 //https://edabit.com/challenge/GBKphScsmDi9ek3ra
 public class HowManyVowels11_15_21 {
 
     public static int getCount(String str) {
         int vowelsCount = 0;
         // Your code here
+        String[] vowels = {"a", "e", "i", "o", "u"};
+
+        String[] newString = str.split("");
+
+        for (String letter : newString) {
+            for (String vowel : vowels) {
+                if (letter.equalsIgnoreCase(vowel)) {
+                    vowelsCount++;
+                }
+            }
+        }
 
         return vowelsCount;
 
