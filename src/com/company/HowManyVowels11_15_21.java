478d380457d398151a6f2f5407232c2026fbc106
TBlais10 <blaislighting@gmail.com>
2021-11-15 18:25:07 -0500

    Boomberang algorithm complete

---
+++ src/com/company/HowManyVowels11_15_21.java
@@ -0,0 +1,16 @@
+package com.company;
+//https://edabit.com/challenge/GBKphScsmDi9ek3ra
+public class HowManyVowels11_15_21 {
+
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+        // Your code here
+
+        return vowelsCount;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/NumberOfBoomerang11_15_21.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//https://edabit.com/challenge/b7iHQDw72zzkmgCun
+public class NumberOfBoomerang11_15_21 {
+
+    //if !arr.length %3 ... if arr.length is not divisible by 3.
+    //if i == i + 1, dont count.
+    //if i == i + 2
+    public static int countBoomerangs(int[] arr) {
+
+        ArrayList<int[]> allBoomerangs = new ArrayList<>();
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            int[] boomerang = new int[3];
+            if (arr[i] == arr[i + 2] && !(arr[i] == arr[i + 1])) {
+                boomerang[0] = arr[i];
+                boomerang[1] = arr[i + 1];
+                boomerang[2] = arr[i + 2];
+                allBoomerangs.add(boomerang);
+            }
+        }
+
+//        if (arr.length != arr.length % 3) {
+//            for (int i = arr.length - 1; i >= 2; i--) {
+//                int[] boomerang = new int[3];
+//                if (arr[i] == arr[i - 2] && !(arr[i] == arr[i - 1])) {
+//                    boomerang[0] = arr[i];
+//                    boomerang[1] = arr[i - 1];
+//                    boomerang[2] = arr[i - 2];
+//                    allBoomerangs.add(boomerang);
+//                }
+//            }
+//
+//        }
+        for (int[] boomberang : allBoomerangs) {
+            System.out.print(Arrays.toString(boomberang) + " - ");
+        }
+        return allBoomerangs.size();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(countBoomerangs(new int[]{9, 5, 9, 5, 1, 1, 1}));
+    }
+}
