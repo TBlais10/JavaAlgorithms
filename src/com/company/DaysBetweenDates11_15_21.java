6433a74da0b7cac2337babcbee0ad965b2c6b173
TBlais10 <blaislighting@gmail.com>
2021-11-20 15:47:21 -0500

    Began working on the time algorithm that was not finished durring the week. More needs to be done to understand these date class and converting the Month string into an int to be read by the program.

--- src/com/company/DaysBetweenDates11_15_21.java
+++ src/com/company/DaysBetweenDates11_15_21.java
@@ -1,12 +1,30 @@
 package com.company;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.Date;
 
 //https://edabit.com/challenge/3hdXjfJozQySRC3gE
 public class DaysBetweenDates11_15_21 {
 
-    public Date getDays (Date date1, Date date2){
+    public static int getDays(String date1, String date2){
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm dd, yyyy");
+        LocalDate firstDate = LocalDate.parse(date1);
+        LocalDate secondDate = LocalDate.parse(date2);
+
+        return (int) ChronoUnit.DAYS.between(firstDate, secondDate);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(getDays("June 14, 2019", "June 20, 2019"));
+        System.out.println(getDays("December 29, 2019", "January 1, 2020"));
     }
 
 }
