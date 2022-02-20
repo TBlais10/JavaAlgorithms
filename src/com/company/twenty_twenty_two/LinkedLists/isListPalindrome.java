d1563fc2c44d16814b540d1eaf30ea948ec1a98c
TBlais10 <blaislighting@gmail.com>
2022-02-19 23:40:21 -0500

    Continuing to solve the algorithm isListPalindrome. Only 2 hidden test cases failed.

--- src/com/company/twenty_twenty_two/LinkedLists/isListPalindrome.java
+++ src/com/company/twenty_twenty_two/LinkedLists/isListPalindrome.java
@@ -1,32 +1,39 @@
 package com.company.twenty_twenty_two.LinkedLists;
 
+import java.util.List;
+import java.util.Objects;
+
 public class isListPalindrome {
 
     // Singly-linked lists are already defined with this interface:
- class ListNode<T> {
-   ListNode(T x) {
-     value = x;
-   }
-   T value;
-   ListNode<T> next;
- }
-//
+    class ListNode<T> {
+        ListNode(T x) {
+            value = x;
+        }
+
+        T value;
+        ListNode<T> next;
+    }
+
     boolean solution(ListNode<Integer> l) {
+        if (l == null) {
+            return true;
+        }
+
         ListNode<Integer> traveler = l;
         int count = 0;
 
-        while (l != null){
-            count++;
-        }
 
-        while (traveler.next != null){
+        while (traveler.next != null) { //Changes l to the last node in the list.
+            count++;
             traveler = traveler.next;
         }
 
+        if (!Objects.equals(l.value, traveler.value)) {
+            return false;
+        }
 
-
-
-     return true;
+        return true;
     }
 
 
