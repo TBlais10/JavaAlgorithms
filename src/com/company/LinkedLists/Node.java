ed818eaf5a98c36c8573db78326f7f634229a57c
TBlais10 <blaislighting@gmail.com>
2022-02-22 22:44:37 -0500

    Continuing to progress with merginLists algorithm and notes on link nodes

---
+++ src/com/company/LinkedLists/Node.java
@@ -0,0 +1,16 @@
+package com.company.LinkedLists;
+
+public class Node<T> {
+    public T value;
+    public Node<T> next;
+//    public Node<T> prev;
+//
+//    public Node(T value) {
+//        this.value = value;
+//        prev = parent;
+//    }
+
+    public Node(T value) {
+        this.value = value;
+    }
+}
--- src/com/company/twenty_twenty_two/LinkedLists/mergeTwoLinkedLists.java
+++ src/com/company/twenty_twenty_two/LinkedLists/mergeTwoLinkedLists.java
@@ -5,42 +5,56 @@ import java.util.List;
 
 public class mergeTwoLinkedLists {
 
-//     Singly-linked lists are already defined with this interface:
- class ListNode<T> {
-   ListNode(T x) {
-     value = x;
-   }
-   T value;
-   ListNode<T> next;
- }
+    //     Singly-linked lists are already defined with this interface:
+    class ListNode<T> {
+        ListNode(T x) {
+            value = x;
+        }
+
+        T value;
+        ListNode<T> next;
+    }
 
     ListNode<Integer> solution(ListNode<Integer> l1, ListNode<Integer> l2) {
-     if (l1 == null && l2 == null){
-         return l1;
-     }
+        if (l1 == null && l2 == null) {
+            return l1;
+        }
 
-     if (l1 == null){
-         return l2;
-     }
+        if (l1 == null) {
+            return l2;
+        }
 
-     if (l2 == null){
-         return l1;
-     }
+        if (l2 == null) {
+            return l1;
+        }
 
-     List<Integer> merged = new ArrayList<>();
-     ListNode<Integer> t1 = l1;
-     ListNode<Integer> t2 = l2;
+        ListNode<Integer> merged = null;
+        ListNode<Integer> t1 = l1;
+        ListNode<Integer> t2 = l2;
 
-     while (t1 != null && t2 != null){
-         if (t1.value >= t2.value){
-             merged.add(t2.value);
-             merged.add(t1.value);
-         }
-     }
+        while (true) {
+            if (t1.value >= t2.value) {
+                //add t1
+                merged = l1;
+                merged.next = solution(l1.next, l2);
+            } else {
+                //add t2
+                merged = l2;
+                merged.next = solution(l1, l2.next);
+            }
+            return merged;
+        }
 
-        System.out.println(merged);
-
-    return (ListNode<Integer>) merged;
     }
+
+//    public static ListNode<Integer> sort(ListNode<Integer> head){
+//        if (head == null || head.next == null){
+//            return head;
+//        }
+//
+//        ListNode<Integer>[] array =
+//
+//        return sort()
+//    }
 
 }
