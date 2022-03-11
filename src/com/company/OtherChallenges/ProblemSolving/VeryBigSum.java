efddadf469bf781b2068fbadd31c79df8fd137cc
TBlais10 <blaislighting@gmail.com>
2022-03-10 22:11:42 -0500

    Solved the Very Big Sum Algorithm.

---
+++ src/com/company/OtherChallenges/ProblemSolving/VeryBigSum.java
@@ -0,0 +1,16 @@
+package com.company.OtherChallenges.ProblemSolving;
+
+import java.util.List;
+
+public class VeryBigSum {
+
+    public static long aVeryBigSum(List<Long> ar) {
+        long answer = 0;
+
+        for (Long num :
+                ar) {
+            answer += num;
+        }
+        return answer;
+    }
+}
