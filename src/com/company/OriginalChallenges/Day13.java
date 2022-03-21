684271007a5c5bfbc80e674d91fb0eb4bb212862
TBlais10 <blaislighting@gmail.com>
2022-03-21 18:10:09 -0400

    Completed Day 13 of 30 Days

--- src/com/company/OriginalChallenges/Day13.java
+++ src/com/company/OriginalChallenges/Day13.java
@@ -1,4 +1,51 @@
 package com.company.OriginalChallenges;
 
+// Declare your class here. Do not use the 'public' access modifier.
+// Declare the price instance variable
+
+
 public class Day13 {
+
+    abstract class Book {
+        String title;
+        String author;
+
+        Book(String title, String author) {
+            this.title = title;
+            this.author = author;
+        }
+
+        abstract void display();
+    }
+
+    /**
+     * Class Constructor
+     *
+     * @param title  The book's title.
+     * @param author The book's author.
+     * @param price  The book's price.
+     **/
+    class MyBook extends Book {
+
+        int price;
+
+        MyBook(String title, String author, int price) {
+            super(title, author);
+            this.price = price;
+        }
+
+        public void display() {
+            System.out.println("Title: " + title + "\nAuthor: " + author + "\nPrice: " + price);
+        }
+    }
+/**
+ *   Method Name: display
+ *
+ *   Print the title, author, and price in the specified format.
+ **/
+// Write your method here
+
+// End class
+
+
 }
