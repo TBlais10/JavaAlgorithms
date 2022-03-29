6ec3bb49359b6a0d26774036bac1f067f8275e72
TBlais10 <blaislighting@gmail.com>
2022-03-28 22:18:32 -0400

    Another attempt at Climbing the leaderboard solution

--- src/com/company/OtherChallenges/ClimbingLeaderboard.java
+++ src/com/company/OtherChallenges/ClimbingLeaderboard.java
@@ -5,34 +5,36 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+/*NOTES FOR ALGORITHM
+ * 2D Loop
+ *
+ * Going thru the ranked board each item..wasting time.
+ * */
+//        for (int i = 0; i < ranked.size(); System.out.println(i)) {
+//            ranked.set(i, ranked.get(i) * i);
+//            i++;
+//        }
+//        System.out.println(ranked);
+//code flow
+
+//        return answer;
+
 // TODO: 3/8/2022 Finish algorithm
 //https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
 public class ClimbingLeaderboard {
 
     public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
-        // Write your code here
-
         List<Integer> answer = new ArrayList<>();
+        int count = 0;
 
-        for (int currentPlayer : player) {
-
-            int count = 0;
-            for (int j = 0; j < ranked.size(); j++) {
-
-                int currentRank = ranked.get(j);
-
-                if (currentPlayer >= currentRank) {
-                    count = j;
+        for (int i = player.size() - 1; i > 0; i--) {
+            for (int j = 0; j < ranked.size(); System.out.println(j)) {
+                if (player.get(i) >= ranked.get(j)) {
+                    answer.add(j);
                     break;
                 }
-
+                j++;
             }
-            if (currentPlayer <= ranked.get(ranked.size() - 1)) {
-                answer.add(ranked.size() -1);
-            } else {
-                answer.add(count);
-            }
-
         }
         return answer;
     }
