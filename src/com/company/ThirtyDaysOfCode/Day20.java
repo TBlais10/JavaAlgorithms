83294ae7bb9adbe0fbaab8f619075fb3490e8875
TBlais10 <blaislighting@gmail.com>
2022-06-08 23:31:39 -0400

    Suggested solution for Day 20. Need to check code block of challenge for correct submission.

---
+++ src/com/company/ThirtyDaysOfCode/Day20.java
@@ -0,0 +1,34 @@
+package com.company.ThirtyDaysOfCode;
+
+/*NOTES
+* Bubble sort is involved*/
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Day20 {
+    
+    static String bubbleSort(List<Integer> arr) {
+        int count = 0;
+
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr.get(i) != i + 1){
+                int track = i;
+                while (arr.get(i) != i + 1){
+                    track++;
+                }
+                int temp = arr.get(track);
+                arr.get(track).equals(arr.get(i));
+                arr.get(i).equals(temp);
+                count++;
+            }
+            
+        }
+        return "Array is sorted in " + count + " swaps. \nFirst Element: " + arr.get(0) + "\nLast Element: " + arr.get(arr.size());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bubbleSort(Arrays.asList(4, 3, 2, 1)));
+    }
+    
+}
